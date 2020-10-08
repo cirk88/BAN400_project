3d09deb98989e22e12e626d2abaa3fcfdaf87df6
Chris Kristiansen <chris.kristiansenrgmail.com>
2020-10-08 12:45:28 +0200

    fixed some writings

--- Function_script.R
+++ Function_script.R
@@ -1,6 +1,16 @@
-# TEST
 
-# this is joao's new writting. 
+library(tidyverse)
+
+# Loading data
+
+# df_fake <- read_csv(unzip("Fake.zip", "Fake.csv"))
+# df_true <- read_csv(unzip("True.zip", "True.csv"))
+
+# df_fake <- saveRDS(df_fake, "Fake.rds")
+# df_true <- saveRDS(df_true, "True.rds")
 
 
-# Test2
+df_fake <- readRDS("Fake.rds")
+df_true <- readRDS("True.rds")
+
+
